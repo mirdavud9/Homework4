5afa46f7739a892be5e11e0b2c0c93c48947084a
mirdavud9 <mirdavudvm-bp217@code.edu.az>
2024-10-10 12:45:00 +0400

    _initial

---
+++ Homework4/Program.cs
@@ -0,0 +1,62 @@
+﻿#region Method Task:GetMin adli method yaradılır. Daxil olunan arrayın ən kiçik elementini geri qaytarir(return).
+//int[] Array = { 15, 22, 3, 44, 9, 18, 7 };
+
+//int minElement = GetMin(Array);
+//Console.WriteLine("Arrayin 3n kicik elementi: " + minElement);
+
+//    static int GetMin(int[] array)
+//{
+//    int min = array[0]; 
+//    for (int i = 1; i < array.Length; i++)
+//    {
+//        if (array[i] < min)
+//        {
+//            min = array[i];
+//        }
+//    }
+
+//    return min;
+//} 
+#endregion
+#region  Overload/Area deyə Method(lar) yaradılır.
+//class Program
+//{
+//    static void Main()
+//    {
+//        double radius = 2;
+//        double circleArea = Area(radius); 
+//        Console.WriteLine("S(cevre)=" + circleArea);
+
+//        double a = 5, b = 10;
+//        double rectangleArea = Area(a, b);
+//        Console.WriteLine("S(duzbucaqli)=" + rectangleArea);
+
+//        double h = 3;
+//        double parallelepipedArea = Area(a, b, h);
+//        Console.WriteLine("S(paralelpiped)=" + parallelepipedArea);
+
+//        double triangleArea = Area(a, b, h, radius); 
+//        Console.WriteLine("S(ucbucaq daxiline cekilmis cevre)=" + triangleArea);
+//    }
+
+//    static double Area(double r)
+//    {
+//        const double P= 3;
+//        return P * r * r;
+//    }
+//    static double Area(double a, double b)
+//    {
+//        return a * b;
+//    }
+//    static double Area(double a, double b, double h)
+//    {
+//        return ((a+b)/2*h);
+//    }
+//    static double Area(double a, double b, double c, double r)
+//    {
+//        double p = (a + b + c) / 2;
+//        return p * r;
+//    }
+//}
+
+#endregion
